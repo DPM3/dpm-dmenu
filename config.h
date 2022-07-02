e6bc079a5fc7444e10b47171e65a294c7da35a98
DPM <dpm3git@gmail.com>
2022-07-02 17:14:28 +0300

    init

---
+++ config.h
@@ -0,0 +1,33 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int fuzzy = 0;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"sans:size=16"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#161925" },
+	[SchemeSel] = { "#ffffff", "#c10ed2" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeMid] = { "#ffffff", "#192328" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 20;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 37;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 2;
